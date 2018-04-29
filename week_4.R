60aad000223635d2041245bb019955cb28e08573
Ben Andrew <benjamin.andrew@duke.edu>
2018-04-28 22:29:24 -0700

    Two new plots added.

--- week_4.R
+++ week_4.R
@@ -20,8 +20,10 @@ male <- filter(raw_data, gender == "Male") %>%
 merged <- left_join(female, male, by = "occupation") %>%
   select(occupation, female_rank, male_rank, female_pay, male_pay,
          female_num, male_num) %>%
-  mutate(total_num = female_num + male_num,    # Not used for current plot
-         pay_gap = abs(male_pay - female_pay)) # Not used for current plot
+  mutate(total_num = female_num + male_num,
+         female_comp = (female_num * (female_pay/1000)), # in thousands of AUD
+         male_comp = (male_num * (male_pay/1000)), # in thousands of AUD
+         total_comp = male_comp + female_comp) # in thousands of AUD
 
 # Scatter plot
 scatter <- ggplot(data = merged, aes(x = male_pay, y = female_pay)) +
@@ -49,7 +51,64 @@ scatter <- ggplot(data = merged, aes(x = male_pay, y = female_pay)) +
 ggsave("./figures/week_4_scatter.png", plot = scatter,
        width = 8, height = 6)
 
+total_scatter <- ggplot(data = merged, aes(x = total_comp, 
+                                           y = male_pay - female_pay)) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth(method = "loess", color = "red") + 
+  coord_cartesian(xlim = c(400, 100000000),
+                  ylim = c(-100000, 400000)) + 
+  geom_segment(aes(x = 400, xend = 25000000,
+                   y = 0, yend = 0), linetype = 2, color = "blue") + 
+  scale_x_log10(breaks = scales::trans_breaks("log10", 
+                                              function(x) 10^x),
+                labels = scales::trans_format("log10", 
+                                              scales::math_format(10^.x))) + 
+  scale_y_continuous(breaks = c(0, 100000, 200000, 300000, 400000),
+                     labels = c("0", "100", "200", "300", "400")) + 
+  ggtitle(
+    "Gender Pay Discrepancy vs. Total Occupation Compensation in Australia") +
+  labs(x = "log(Total Compensation in Thousands of AUD)",
+       y = "Difference in Average Taxable Income (Men - Women)",
+       caption = "Source: data.gov.au | Graphic by Ben Andrew") +
+  annotate("text", label = "LOESS", color = "red",
+           x = 28000000, y = 25000, size = 3, hjust = 0) + 
+  annotate("text", label = "No Relationship", color = "blue",
+           x = 28000000, y = 0, size = 3, hjust = 0) + 
+  theme_classic() + 
+  theme(plot.caption = element_text(size = 8, hjust = 1),
+        plot.title = element_text(size = 10, face = "bold")) + 
+  annotation_logticks(sides = "b") 
 
+ggsave("./figures/week_4_total_scatter.png", plot = total_scatter,
+       width = 8, height = 6)
 
+prop_scatter <- ggplot(data = merged, aes(x = (female_num / male_num), 
+                          y = male_pay - female_pay)) + 
+  geom_point(alpha = 0.3) + 
+  geom_smooth(method = "loess", color = "red") + 
+  geom_segment(aes(x = 0.0017, xend = 100,
+                   y = 0, yend = 0), linetype = 2, color = "blue") + 
+  coord_cartesian(xlim = c(0.0015, 300),
+                  ylim = c(-100000, 400000)) + 
+  scale_x_log10(breaks = scales::trans_breaks("log10", 
+                                              function(x) 10^x),
+                labels = scales::trans_format("log10", 
+                                              scales::math_format(10^.x))) + 
+  scale_y_continuous(breaks = c(0, 100000, 200000, 300000, 400000),
+                     labels = c("0", "100", "200", "300", "400")) + 
+  ggtitle(
+    "Gender Pay Discrepancy vs. Occupation Gender Composition") +
+  labs(x = "log(Proportion of Female Employees in Workforce)",
+       y = "Difference in Average Taxable Income (Men - Women)",
+       caption = "Source: data.gov.au | Graphic by Ben Andrew") +
+  annotate("text", label = "LOESS", color = "red",
+           x = 120, y = 10000, size = 3, hjust = 0) + 
+  annotate("text", label = "No Relationship", color = "blue",
+           x = 120, y = -6000, size = 3, hjust = 0) + 
+  theme_classic() + 
+  theme(plot.caption = element_text(size = 8, hjust = 1),
+        plot.title = element_text(size = 10, face = "bold")) + 
+  annotation_logticks(sides = "b") 
   
-  
+ggsave("./figures/week_4_prop_scatter.png", plot = prop_scatter,
+       width = 8, height = 6)
